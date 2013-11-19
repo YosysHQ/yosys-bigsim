0c06e4db7f9ebc0f8225f494c0322ebc0cc412ac
Clifford Wolf <clifford@clifford.at>
2013-11-19 15:49:43 +0100

    Fixed amber23 testbench wishbone interface

--- amber23/sim/bench.v
+++ amber23/sim/bench.v
@@ -9,8 +9,8 @@ reg i_system_rdy;       // Amber is stalled when this is low
 
 // Wishbone Inputs
 reg [31:0] i_wb_dat;
-reg        i_wb_ack;
-reg        i_wb_err;
+wire       i_wb_ack;
+wire       i_wb_err;
 
 // Wishbone Outputs
 wire [31:0] o_wb_adr;
@@ -49,13 +49,11 @@ initial begin
 	i_irq <= 0;
 	i_firq <= 0;
 	i_wb_dat <= 0;
-	i_wb_ack <= 0;
-	i_wb_err <= 0;
 	i_system_rdy <= 0;
 	repeat (10) @(posedge clk);
 	i_system_rdy <= 1;
-	repeat (20000) @(posedge clk);
-	$display("Reached limit of 20000 cpu cycles.");
+	repeat (5000) @(posedge clk);
+	$display("Reached limit of 5000 cpu cycles.");
 	$finish;
 end
 
@@ -75,23 +73,31 @@ initial begin
 `include "sieve.v"
 end
 
+reg wb_start_read_d1;
+wire wb_start_write = o_wb_stb &&  o_wb_we && !wb_start_read_d1;
+wire wb_start_read  = o_wb_stb && !o_wb_we && !i_wb_ack;
+assign i_wb_ack = o_wb_stb && ( wb_start_write || wb_start_read_d1 );
+assign i_wb_err = 0;
+
+always @(posedge clk)
+	wb_start_read_d1 <= wb_start_read;
+
 always @(posedge clk) begin
-	if (o_wb_cyc && o_wb_stb && !i_wb_ack) begin
+	if (wb_start_write || wb_start_read) begin
 		if (o_wb_adr < mem_size*4) begin
 			tmp = mem[o_wb_adr >> 2];
-			if (o_wb_we) begin
+			if (wb_start_write) begin
 				if (o_wb_sel[0]) tmp = { tmp[31:24], tmp[23:16], tmp[15:8], o_wb_dat[7:0] };
 				if (o_wb_sel[1]) tmp = { tmp[31:24], tmp[23:16], o_wb_dat[15:8], tmp[7:0] };
 				if (o_wb_sel[2]) tmp = { tmp[31:24], o_wb_dat[23:16], tmp[15:8], tmp[7:0] };
 				if (o_wb_sel[3]) tmp = { o_wb_dat[31:24], tmp[23:16], tmp[15:8], tmp[7:0] };
 				mem[o_wb_adr >> 2] <= tmp;
 			end
+			if (wb_start_read)
+				i_wb_dat <= tmp;
 			$display("%t MEM %s ADDR %08x: %08x (%b)", $time, o_wb_we ? "WRITE" : "READ ", o_wb_adr, tmp, o_wb_sel);
-			i_wb_dat <= tmp;
-			i_wb_ack <= 1;
-			i_wb_err <= 0;
-		end else
-		if (o_wb_adr == 32'h10000000 && o_wb_we) begin
+		end
+		if (wb_start_write && o_wb_adr == 32'h10000000) begin
 			$display("%t OUTPUT %d", $time, o_wb_dat);
 			if (o_wb_dat == 0) begin
 				-> output_eof;
@@ -99,15 +105,7 @@ always @(posedge clk) begin
 				output_buf[output_idx] = o_wb_dat;
 				output_idx = output_idx + 1;
 			end
-			i_wb_ack <= 1;
-			i_wb_err <= 0;
-		end else begin
-			i_wb_ack <= 1;
-			i_wb_err <= 1;
 		end
-	end else begin
-		i_wb_ack <= 0;
-		i_wb_err <= 0;
 	end
 end
 
